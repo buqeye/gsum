27303fc7edf7a374517fbbc76a2e83cd910de41d
jordan-melendez <jmelendez1992@gmail.com>
2017-09-19 09:02:06 -0400

    Add license and fill in setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='buqeyemodel',
+    packages=['buqeyemodel'],
+    version='0.1.0',
+    description='A statistical model of EFT convergence.',
+    author='Jordan Melendez',
+    author_email='jmelendez1992@gmail.com',
+    url='https://github.com/jordan-melendez/buqeyemodel',
+    # download_url='',
+    keywords=[
+        'EFT', 'nuclear', 'model', 'gaussian', 'process',
+        'uncertainty', 'quantification'
+        ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics'
+        ]
+)
