f57806a06235ae20c814f55a5158aa7718fc6dd4
jordan-melendez <jmelendez1992@gmail.com>
2018-12-01 19:37:16 -0500

    Now can use build_ext for cython

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+from Cython.Build import cythonize, build_ext
+import numpy
+import cython_gsl
+
+extensions = [Extension(
+    name="gsum.cutils", 
+    sources=["gsum/cutils.pyx"],
+    # depends=['gsl/gsl_linalg.h', 'gsl/gsl_permutation.h'],
+    libraries=cython_gsl.get_libraries(),
+    library_dirs=[cython_gsl.get_library_dir()],
+    include_dirs=[numpy.get_include(), cython_gsl.get_cython_include_dir()],
+)]
 
 setup(
     name='gsum',
     packages=['gsum'],
     # py_modules=[''],
+    include_dirs=[cython_gsl.get_include()],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=cythonize(extensions, build_dir='gsum'),
     version='0.1',
     description='A Bayesian model of series convergence using Gaussian sums',
     author='Jordan Melendez',
