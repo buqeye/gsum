6f556f599f71c67f0eacfe2729c212e9de1812c6
jordan-melendez <jmelendez1992@gmail.com>
2019-04-18 15:10:24 -0400

    Added required steps for building documentation, since it is unnecessary for requirements.txt

--- build_and_deploy_docs.sh
+++ build_and_deploy_docs.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Requires the following:
+# -----------------------
+# pip install ghp-import
+# conda install sphinx
+# pip install guzzle_sphinx_theme
+# pip install numpydoc
+# pip install nbsphinx
+# pip install sphinxcontrib-inlinesyntaxhighlight
+# conda install ipython
+# conda install -c conda-forge pandoc=1.19.2
+
 pushd docs
 make html
 ghp-import -n -p _build/html/
