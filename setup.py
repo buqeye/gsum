1229645d8d4af58bcb4f3a524297cad94c22bc46
jordan-melendez <jmelendez1992@gmail.com>
2019-09-04 12:47:22 -0400

    Added install_requires and fixed install when cython or numpy are not yet installed

--- setup.py
+++ setup.py
@@ -1,26 +1,54 @@
 from distutils.core import setup, Extension
-from Cython.Build import cythonize, build_ext
-import numpy
-import cython_gsl
 
-# Run: python setup.py build_ext --build-lib ./
+# To build files manually, run: python setup.py build_ext --build-lib ./
 
-extensions = [Extension(
-    name="gsum.cutils", 
-    sources=["gsum/cutils.pyx"],
-    # depends=['gsl/gsl_linalg.h', 'gsl/gsl_permutation.h'],
-    libraries=[*cython_gsl.get_libraries()],
-    library_dirs=[cython_gsl.get_library_dir()],
-    include_dirs=[numpy.get_include(), cython_gsl.get_cython_include_dir()],
-)]
+# extensions = [Extension(
+#     name="gsum.cutils",
+#     sources=["gsum/cutils.pyx"],
+#     # depends=['gsl/gsl_linalg.h', 'gsl/gsl_permutation.h'],
+#     # libraries=[*cython_gsl.get_libraries()],
+#     # library_dirs=[cython_gsl.get_library_dir()],
+#     # include_dirs=[numpy.get_include(), cython_gsl.get_cython_include_dir()],
+# )]
+
+# Do not import packages if they are not yet installed via install_requires
+try:
+    from Cython.Build import cythonize, build_ext
+except ImportError:
+    # If we couldn't import Cython, use the normal setuptools
+    # and look for a pre-compiled .c file instead of a .pyx file
+    from setuptools.command.build_ext import build_ext
+    ext_modules = [Extension("gsum.cutils", ["gsum/cutils.c"])]
+else:
+    # If we successfully imported Cython, look for a .pyx file
+    ext_modules = cythonize([Extension("gsum.cutils", ["gsum/cutils.pyx"])])
+
+
+class CustomBuildExtCommand(build_ext):
+    """build_ext command for use when numpy/cython_gsl headers are needed."""
+    def run(self):
+
+        # Import numpy here, only when headers are needed
+        import numpy
+        import cython_gsl
+
+        # Add the headers
+        self.libraries = cython_gsl.get_libraries()
+        self.library_dirs.append(cython_gsl.get_library_dir())
+
+        self.include_dirs.append(numpy.get_include())
+        self.include_dirs.append(cython_gsl.get_include())
+        self.include_dirs.append(cython_gsl.get_cython_include_dir())
+
+        # Call original build_ext command
+        build_ext.run(self)
+
 
 setup(
     name='gsum',
     packages=['gsum'],
-    # py_modules=[''],
-    include_dirs=[cython_gsl.get_include()],
-    cmdclass={'build_ext': build_ext},
-    ext_modules=cythonize(extensions, build_dir='gsum'),
+    cmdclass={'build_ext': CustomBuildExtCommand},
+    ext_modules=ext_modules,
     version='0.1',
     description='A Bayesian model of series convergence using Gaussian sums',
     author='Jordan Melendez',
@@ -39,5 +67,18 @@ setup(
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Physics'
-        ]
+        ],
+    install_requires=[
+        'Cython',
+        'CythonGSL',
+        'docrep',
+        'gsl',
+        'numpy>=1.12.0',
+        'pandas',
+        'scipy',
+        'seaborn',
+        'statsmodels',
+        'matplotlib',
+        'scikit-learn',
+    ]
 )
